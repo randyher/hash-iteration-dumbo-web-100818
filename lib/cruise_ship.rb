11796f1626b6fcb0419138040d6aab2e437371f3
Randy Herasme <randyherasme6@gmail.com>
2018-09-18 19:12:50 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,12 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner=""
+  passengers.each do |suite, name|
+    if (suite==:suite_a && name[0]=="A")
+      winner=name
+      puts 
+    end
+  end
+    winner
 end
